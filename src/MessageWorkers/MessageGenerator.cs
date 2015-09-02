8f6ac4f40c896a288d774eb377788e30f2b57730
Xavier Decoster <xavier.decoster@outlook.com>
2015-09-02 19:19:02 +0200

    messagebroker observables now ensure subscribers handle messages on or off the message request thread

---
+++ src/MessageWorkers/MessageGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageWorkers
+{
+    public class MessageGenerator
+    {
+        public MessageGenerator(MessageBroker messageBroker)
+        {
+            MessageBroker = messageBroker;
+            Random = new Random();
+        }
+
+        private Random Random { get; }
+
+        private MessageBroker MessageBroker { get; }
+
+        public void Start()
+        {
+            var count = 0;
+            for (var i = 0; i < 2; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        var sleep = Random.Next(1000, 2000);
+                        //var sleep = Random.Next(5000, 6000);
+                        var description = $"Item {count++} created on Thread {Thread.CurrentThread.ManagedThreadId} - (sleep {sleep})";
+
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}] Generated - \"{description}\"");
+
+                        var onRequestThread = sleep % 4 == 0; // run randomly on/off request thread (ca. 25% on request thread, 75% off request thread)
+
+                        MessageBroker.SendMessage(new Message(description, Thread.CurrentThread.ManagedThreadId), onRequestThread);
+                        Thread.Sleep(sleep);
+                    }
+                });
+            }
+        }
+    }
+}
--- src/MessageWorkers/Program.cs
+++ src/MessageWorkers/Program.cs
@@ -1,143 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Concurrency;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MessageWorkers
 {
     public class Program
     {
+        private readonly object _consoleLock = new object(); // locking console coloring :)
+
         public void Main(string[] args)
         {
-
             var messageBroker = new MessageBroker();
             var messageGenerator = new MessageGenerator(messageBroker);
 
             // On Request thread subscribers
             messageBroker.OnRequestThread.Subscribe(message =>
             {
-                Console.WriteLine($"On Req Observable     - Thread {Thread.CurrentThread.ManagedThreadId}     - \"{message.Description}\"");
-            });
-            messageBroker.OnRequestThread.Subscribe(message =>
-            {
-                // Basic test to make sure that observable doesn't run only one message as once.
-                Thread.Sleep(Thread.CurrentThread.ManagedThreadId % 2 == 0 ? 20000 : 50);
-            });
-
-            // Off Request thread subscribers
-            var stall = true;
-            messageBroker.OffRequestThread.Subscribe(message =>
-            {
-                Console.WriteLine($"Off Req Observable    - Thread {Thread.CurrentThread.ManagedThreadId}     - \"{message.Description}\"");
-
-                // QUESTION: This blocks until the minute is up from any other workers eating
-                //           away at the queue, I would have thought that leveraging TaskPoolScheduler
-                //           would mean that other workers/threads would pickup on the messages backingup 
-                if (stall)
+                var subscriberThreadId = Thread.CurrentThread.ManagedThreadId;
+                lock (_consoleLock)
                 {
-                    stall = false;
-                    Thread.Sleep(60000);
+                    // ON Request means subscriber and observable must share thread id...
+                    Console.ForegroundColor = subscriberThreadId == message.CreatedOnThreadId ? ConsoleColor.Yellow : ConsoleColor.Red;
+                    Console.WriteLine($"[{subscriberThreadId}] ON Request processing - \"{message.Description}\"");
                 }
             });
 
-
+            // Off Request thread subscribers
+            messageBroker.OffRequestThread.Subscribe(message =>
+            {
+                var subscriberThreadId = Thread.CurrentThread.ManagedThreadId;
+                lock (_consoleLock)
+                {
+                    // OFF Request means subscriber and observable cannot share thread id...
+                    Console.ForegroundColor = subscriberThreadId != message.CreatedOnThreadId ? ConsoleColor.White : ConsoleColor.Red;
+                    Console.WriteLine($"[{subscriberThreadId}] OFF Request processing - \"{message.Description}\"");
+                }
+            });
 
             messageGenerator.Start();
 
             Console.ReadLine();
         }
-        
-    }
-
-    public class MessageBroker
-    {
-        private readonly ISubject<Message> _onRequestThreadSubject;
-        private readonly ISubject<Message> _offRequestThreadSubject; 
-
-        public MessageBroker()
-        {
-            _onRequestThreadSubject = new Subject<Message>();
-            _offRequestThreadSubject = new Subject<Message>();
-
-            // bring the data from the request thread onto a different thread
-            // TODO: Instead of having this as one of the on thread subscribers, should spawn in SendMessage
-            _onRequestThreadSubject.ObserveOn(TaskPoolScheduler.Default).Subscribe(message =>
-            {
-                _offRequestThreadSubject.OnNext(message);
-            });
-        }
-
-        public IObservable<Message> OnRequestThread
-        {
-            get { return _onRequestThreadSubject; }
-        }
-
-        public IObservable<Message> OffRequestThread
-        {
-            get { return _offRequestThreadSubject; }
-        }
-
-        public void SendMessage(Message message)
-        {
-            // Question: Is this blocking? Given that MessageBroker is a signelton
-            //           does it mean what only one message at a time can go through
-            //           the observable pipeline
-            // Tests: Current tests should that subscribers run on the same thread 
-            //        that generated the message, this would imply that its ok?
-            _onRequestThreadSubject.OnNext(message);
-        }
-    }
-
-    public class MessageGenerator
-    {
-        public MessageGenerator(MessageBroker messageBroker)
-        {
-            MessageBroker = messageBroker;
-            Random = new Random();
-        }
-
-        private Random Random { get; }
-
-        private MessageBroker MessageBroker { get; }
-
-        public void Start()
-        {
-            var count = 0;
-            for (var i = 0; i < 2; i++)
-            {
-                Task.Factory.StartNew(() =>
-                {
-                    while (true)
-                    {
-                        //var sleep = Random.Next(1000, 2000);
-                        var sleep = Random.Next(5000, 6000);
-                        var description = $"Item {count++} - Thread {Thread.CurrentThread.ManagedThreadId} (sleep {sleep})";
-
-                        Console.WriteLine($"Generated             - Thread {Thread.CurrentThread.ManagedThreadId}     - \"{description}\"");
-
-                        MessageBroker.SendMessage(new Message(description));
-                        Thread.Sleep(sleep);
-                    }
-                });
-            }
-        }
-    }
-
-    public class Message
-    {
-        public Message(string description)
-        {
-            Description = description;
-        }
-
-        public Guid Id => Guid.NewGuid();
-
-        public DateTime DateTime => DateTime.Now;
-
-        public string Description { get; }
     }
 }
