9ae619e34d64925953967ac796229d706acc6ef2
Ryan Nowak <nowakra@gmail.com>
2015-09-02 23:25:33 -0700

    Add another sample

---
+++ src/MessageWorkers2/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MessageWorkers2
+{
+    public class Message
+    {
+        public Message(string description)
+        {
+            Description = description;
+        }
+
+        public Guid Id => Guid.NewGuid();
+
+        public DateTime DateTime => DateTime.Now;
+
+        public string Description { get; }
+    }
+}
